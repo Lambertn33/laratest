1e06f9633242c005cbe637ae24b378ae71e84647
Lambertn33 <lambertgithub@gmail.com>
2024-01-03 12:10:00 +0200

    feat: perform tests on test DB

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,20 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    public function test_products_page_returns_empty_products(): void
     {
         $response = $this->get('/products');
 
         $response->assertSee('No Products available');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_products_page_dont_returns_empty_products(): void
+    {
+        Product::create([
+            'name' => 'Product 1',
+            'description' => 'Product 1 description',
+            'price' => 12000
+        ]);
+
+        $response = $this->get('/products');
+
+        $response->assertDontSee('No Products available');
 
         $response->assertStatus(200);
     }
