c58e43452a398abdcd8b0aa78211245fc38609ca
Lambertn33 <lambertgithub@gmail.com>
2024-01-03 16:25:38 +0200

    feat: add auth test

--- tests/Feature/Auth/AuthTest.php
+++ tests/Feature/Auth/AuthTest.php
@@ -2,19 +2,36 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+    
+    public function test_user_can_login(): void
     {
-        $response = $this->get('/');
+        User::create([
+            'name' => 'lamb',
+            'email' => 'lamb@gmail.com',
+            'password' => Hash::make('lamb12345')
+        ]);
 
-        $response->assertStatus(200);
+        $response = $this->post('/login', [
+            'email' => 'lamb@gmail.com',
+            'password' => 'lamb12345'
+        ]);
+
+        $response->assertRedirectToRoute('products.index');
+    }
+
+    public function test_user_cannot_access_private_page_without_login(): void
+    {
+        $response = $this->get('/products');
+
+        $response->assertRedirect('/login');
     }
 }
